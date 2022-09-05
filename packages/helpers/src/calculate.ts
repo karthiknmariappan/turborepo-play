97bdfc4d759c8db53505e944b9df6a73a267fdc5
Karthikeyan Mariappan <karthikeyan.mariappan@bowbridge.net>
2022-09-05 11:32:14 +0200

    feat: :sparkles: more methods

--- packages/helpers/src/calculate.ts
+++ packages/helpers/src/calculate.ts
@@ -2,3 +2,12 @@ export const add = (a: number, b: number): number => {
 
     return a + b
 }
+
+export const sub = (a: number, b: number): number => {
+    return a - b
+}
+
+export const multiply = (a: number, b: number): number => {
+
+    return a * b
+}
